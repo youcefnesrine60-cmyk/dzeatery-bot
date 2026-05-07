c7c7229ae65fe783865a03255268d496ddd3a941
youcefnesrine60-cmyk <youcefnesrine60@gmail.com>
2026-05-07 13:17:39 +0100

    update project

--- app/config.py
+++ app/config.py
@@ -5,4 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+class Config:
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    DATABASE_URL = os.getenv("DATABASE_URL")
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
